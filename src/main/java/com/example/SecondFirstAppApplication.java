7bd962721b71a35ef5794cdcfe84609d5af4cfae
Tibor Molnar <tibor.molnar@suitsolutions.eu>
2018-03-26 11:17:01 +0200

    it works

---
+++ src/main/java/com/example/SecondFirstAppApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondFirstAppApplication.class, args);
+	}
+}
